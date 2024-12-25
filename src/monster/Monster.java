8a15e237d73bfc896e63aeb3d0783781a9092d77
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2024-12-26 01:04:10 +0900

    feat(Monster): MonsterImpl
    - Monster class 구현
    - damage 를 받고 Hp 가 0 이하일 경우 gold 생성 및 Monster levelUp
    구현

--- src/monster/Monster.java
+++ src/monster/Monster.java
@@ -5,4 +5,5 @@ public interface Monster {
     void beAttacked(long damage);
     boolean isAlive();
     void levelUp();
+    long yieldGold();
 }
--- src/monster/MonsterImpl.java
+++ src/monster/MonsterImpl.java
@@ -3,16 +3,18 @@ package monster;
 public class MonsterImpl implements Monster {
     private long maxHp;
     private long hp; // current hp
-    private float HpIncreaseRate = 1.55F;
+    private float HP_INCREASE_RATE = 1.55F;
+    private long level;
 
     public MonsterImpl() {
         // initial monster
         this.maxHp = 10;
         this.hp = this.maxHp;
+        this.level = 1;
     }
 
     private long increaseHp() {
-        return (long) Math.ceil(maxHp * HpIncreaseRate);
+        return (long) Math.ceil(maxHp * HP_INCREASE_RATE);
     }
 
     @Override
@@ -34,5 +36,16 @@ public class MonsterImpl implements Monster {
     public void levelUp() {
         this.maxHp = increaseHp();
         this.hp = this.maxHp;
+        this.level++;
+    }
+
+    @Override
+    public long yieldGold() {
+        return (long) (maxHp / 10 * Math.min(3, Math.pow(1.025f, Math.max(0, level-75))));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("monster hp: %d, level: %d", hp, level);
     }
 }
