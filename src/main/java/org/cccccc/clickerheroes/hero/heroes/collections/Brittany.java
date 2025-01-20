5fa1a84957c4a5bdfbc7990e9704a5f1c0bf6fe4
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2025-01-21 01:21:51 +0900

    feat(Heroes):
    hero 추가

---
+++ src/main/java/org/cccccc/clickerheroes/hero/heroes/collections/Brittany.java
@@ -0,0 +1,19 @@
+package org.cccccc.clickerheroes.hero.heroes.collections;
+
+import org.cccccc.clickerheroes.hero.AbstractHero;
+
+public class Brittany extends AbstractHero {
+    public Brittany(long damage, long cost) {
+        super(damage, cost);
+    }
+
+    @Override
+    public void globalSkill() {
+
+    }
+
+    @Override
+    public void localSkill() {
+
+    }
+}
---
+++ src/main/java/org/cccccc/clickerheroes/hero/heroes/collections/Fisherman.java
@@ -0,0 +1,19 @@
+package org.cccccc.clickerheroes.hero.heroes.collections;
+
+import org.cccccc.clickerheroes.hero.AbstractHero;
+
+public class Fisherman extends AbstractHero {
+    public Fisherman(long damage, long cost) {
+        super(damage, cost);
+    }
+
+    @Override
+    public void globalSkill() {
+
+    }
+
+    @Override
+    public void localSkill() {
+
+    }
+}
