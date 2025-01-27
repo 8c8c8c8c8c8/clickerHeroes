eff13ff26131e146ee79d53e1071286578f71aad
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2025-01-27 23:48:48 +0900

    feat(Heroes): Collection
    - Hero Ivan 추가

---
+++ src/main/java/org/cccccc/clickerheroes/hero/heroes/collections/Ivan.java
@@ -0,0 +1,19 @@
+package org.cccccc.clickerheroes.hero.heroes.collections;
+
+import org.cccccc.clickerheroes.hero.AbstractHero;
+
+public class Ivan extends AbstractHero {
+    public Ivan(long damage, long cost) {
+        super(damage, cost);
+    }
+
+    @Override
+    public void globalSkill() {
+
+    }
+
+    @Override
+    public void localSkill() {
+
+    }
+}
