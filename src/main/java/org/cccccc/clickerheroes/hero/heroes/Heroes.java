ea60865ad71041ff0ddce7a29523e365c7dce6ca
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2025-02-01 23:02:59 +0900

    feat(Hero): Hero, Heroes
    - Heroes enum 에서 monster 를 attack, active hero status 를 출력 하는 static method 구현
    - index 부여

--- src/main/java/org/cccccc/clickerheroes/hero/heroes/Heroes.java
+++ src/main/java/org/cccccc/clickerheroes/hero/heroes/Heroes.java
@@ -4,26 +4,45 @@ import org.cccccc.clickerheroes.hero.Hero;
 import org.cccccc.clickerheroes.hero.heroes.collections.*;
 import org.cccccc.clickerheroes.monster.Monster;
 
-public enum Heroes {
-    Cid(new Cid(1, 1)),
-    Treebeast(new Treebeast(5, 10)),
-    Ivan(new Ivan(22, 250)),
-    Brittany(new Brittany(74, 1000)),
-    Fisherman(new Fisherman(245, 4000));
-    private final Hero hero;
+import java.util.Arrays;
+import java.util.List;
 
-    Heroes(Hero hero) {
+public enum Heroes {
+    Cid(new Cid(1, 1), 0),
+    Treebeast(new Treebeast(5, 10), 1),
+    Ivan(new Ivan(22, 250), 2),
+    Brittany(new Brittany(74, 1000), 3),
+    Fisherman(new Fisherman(245, 4000), 4);
+    private final Hero hero;
+    private final int index;
+
+    Heroes(Hero hero, int index) {
         this.hero = hero;
-    }
-    public void attack(Monster monster) {
-        hero.attack(monster);
+        this.index = index;
     }
 
     public boolean isActive() {
         return hero.isActive();
     }
+
     @Override
     public String toString() {
         return hero.toString();
+    }
+
+    public static void attack(Monster monster) {
+        Arrays.stream(Heroes.values()).forEach(hero -> {
+            hero.hero.attack(monster);
+        });
+    }
+
+    public static String getHeroStatus() {
+        List<String> activeHeroList = Arrays.stream(Heroes.values())
+                .filter(Heroes::isActive)
+                .map(hero -> {
+                    return String.format("index: %d, name: %s, %s", hero.index, hero.name(), hero.hero);
+                })
+                .toList();
+        return String.join("\n", activeHeroList);
     }
 }
