045b15865287426d7407e76120831d167ad481b4
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2025-02-03 00:55:51 +0900

    feat(javafx): RootController
    - goldstatus, monsterstatus 따로 보여줄 예정, TextField -> Label 로 변경 예정
    - 실제와 비슷하게 HeroList 보여줄 예정(hero name 적힌 Label 과 levelup Button 을 HBox 로 묶고 VBox 에 추가)
    - levelup Button 에 action 추가 예정

--- src/main/java/org/cccccc/clickerheroes/javafx/RootController.java
+++ src/main/java/org/cccccc/clickerheroes/javafx/RootController.java
@@ -1,66 +1,64 @@
 package org.cccccc.clickerheroes.javafx;
 
 import com.google.inject.Inject;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import org.cccccc.clickerheroes.clickerHeroes.ClickerHeroes;
 import org.cccccc.clickerheroes.clickerHeroes.ClickerHeroesTask;
-import org.cccccc.clickerheroes.javafx.cli.CLI;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.stream.IntStream;
 
 public class RootController implements Initializable {
     @FXML
-    private Button enter;
+    private TextField goldStatus;
     @FXML
-    private TextArea gameOutput, cliOutput;
+    private TextField monsterStatus;
     @FXML
-    private TextField cliInput;
+    private VBox heroesList;
     @Inject
     private ClickerHeroes clickerHeroes;
-    private CLI cli;
     private ClickerHeroesTask clickerHeroesTask;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        this.cli = new CLI(clickerHeroes);
         this.clickerHeroesTask = new ClickerHeroesTask(clickerHeroes);
-        enter.setOnAction((event) -> enterUserInput(event));
-        cliInput.setOnKeyPressed((event) -> setCliInputOnEnterPressed(event));
-        runCLI();
+        runGUI();
+        initHeroesList();
         runClickerHeroes();
     }
 
-    private void enterUserInput(ActionEvent event) {
-        cliOutput.appendText("\n" + cliInput.getText());
-        cliInput.clear();
-        cliInput.requestFocus();
+    private void initHeroesList() {
+        IntStream.range(0, 20).forEach(idx -> {
+            generateHeroItem();
+        });
     }
 
-    private void setCliInputOnEnterPressed(KeyEvent event) {
-        if (KeyCode.ENTER.equals(event.getCode())) {
-            enter.requestFocus();
-        }
+    private void generateHeroItem() {
+        HBox container = new HBox();
+        container.setFillHeight(true);
+        container.getStyleClass().add("h1");
+        Label heroInfo = new Label("Cid");
+        heroInfo.getStyleClass().setAll("lbl", "lbl-warning");
+        Button levelUpBtn = new Button("LevelUp");
+        levelUpBtn.getStyleClass().setAll("btn", "btn-warning");
+        container.getChildren().add(heroInfo);
+        container.getChildren().add(levelUpBtn);
+        heroesList.getChildren().add(container);
     }
 
-    private void runCLI() {
-        cliOutput.textProperty().bind(cli.messageProperty());
-        Thread cliTask = new Thread(cli);
-        cliTask.setDaemon(true);
-        cliTask.start();
-        System.out.println(cliTask.getName());
-        System.out.println(Thread.currentThread() + "#########");
+    private void runGUI() {
+
     }
 
     private void runClickerHeroes() {
-        gameOutput.textProperty().bind(clickerHeroesTask.messageProperty());
+        monsterStatus.textProperty().bind(clickerHeroesTask.messageProperty());
         Thread chTask = new Thread(clickerHeroesTask);
         chTask.setDaemon(true);
         chTask.start();
