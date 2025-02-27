44b3f22e7871e4553fca4a0bc6ea3886ae538290
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2025-02-27 16:24:42 +0900

    feat(module-info):
    - export package 추가

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,4 +8,9 @@ module org.cccccc.clickerheroes {
     exports org.cccccc.clickerheroes;
     exports org.cccccc.clickerheroes.javafx;
     opens org.cccccc.clickerheroes.javafx to javafx.fxml;
+    exports org.cccccc.clickerheroes.clickerHeroes;
+    exports org.cccccc.clickerheroes.gold;
+    exports org.cccccc.clickerheroes.monster;
+    exports org.cccccc.clickerheroes.hero.heroes;
+    exports org.cccccc.clickerheroes.datatype;
 }
