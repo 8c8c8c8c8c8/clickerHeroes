bacb6760e1d4a6e7d00682f6fd7328f602f95ed2
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2025-02-27 16:35:07 +0900

    feat(Hero): AbstractHero
    - constructor (damage, cost): String 으로 변경
    - damage, cost: ExpExprProperty type 으로 변경
    - cost formula: Hero 에 관한 ExpExprProperty 를 따로 구현하여 여기에 cost formula 를 옮길 예정

--- src/main/java/org/cccccc/clickerheroes/hero/AbstractHero.java
+++ src/main/java/org/cccccc/clickerheroes/hero/AbstractHero.java
@@ -1,21 +1,23 @@
 package org.cccccc.clickerheroes.hero;
 
 import javafx.scene.control.Label;
+import org.cccccc.clickerheroes.datatype.ExpExprProperty;
 import org.cccccc.clickerheroes.gold.Gold;
+import utils.BindToLabel;
 
-public abstract class AbstractHero implements Hero {
-    private final long BASE_COST;
-    private long baseDamage;
-    private long damage;
+public abstract class AbstractHero implements Hero, BindToLabel {
+    private final ExpExprProperty baseCost;
+    private final ExpExprProperty baseDamage;
 
     protected final double LEVEL_UP_COST_INC_RATIO = 1.07f;
     protected int level;
+    protected ExpExprProperty cost;
 
-    public AbstractHero(long damage, long cost) {
-        this.baseDamage = damage;
+    public AbstractHero(String damage, String cost) {
+        String heroName = getClass().getSimpleName();
+        this.baseCost = new ExpExprProperty(String.format("%sCost", heroName), cost);
+        this.baseDamage = new ExpExprProperty(String.format("%sDamage", heroName), damage);
         this.level = 0;
-        this.damage = damage;
-        this.BASE_COST = cost;
     }
 
     @Override
@@ -28,12 +30,15 @@ public abstract class AbstractHero implements Hero {
     }
 
     private void updateDamage() {
-        damage = baseDamage * level;
+//        damage = baseDamage * level;
+        // todo
     }
 
     @Override
     public String toString() {
-        return String.format("damage: %d, level: %d", damage, level);
+        // todo
+        return super.toString();
+//        return String.format("damage: %d, level: %d", damage, level);
     }
 
     @Override
@@ -43,9 +48,11 @@ public abstract class AbstractHero implements Hero {
 
     protected long levelUpCostFormula(int level) {
         // C = BASE_COST * (LEVEL_UP_COST_INC_RATIO ^ level - 1) / (LEVEL_UP_COST_INC_RATIO - 1)
-        return BASE_COST * (long) Math.floor(
-                (Math.pow(LEVEL_UP_COST_INC_RATIO, level) - 1) / (LEVEL_UP_COST_INC_RATIO - 1)
-        );
+//        return baseCost * (long) Math.floor(
+//                (Math.pow(LEVEL_UP_COST_INC_RATIO, level) - 1) / (LEVEL_UP_COST_INC_RATIO - 1)
+//        );
+        // todo
+        return 0;
     }
 
     @Override
@@ -61,6 +68,11 @@ public abstract class AbstractHero implements Hero {
 
     @Override
     public void bindToLabel(Label label) {
+        // todo
+    }
+
+    @Override
+    public void addDamageTo(ExpExprProperty obj) {
         // todo
     }
 }
