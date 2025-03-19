60472d36b4de2cbd8937d3c5a238ddf61b5c4340
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2025-03-19 22:34:06 +0900

    feat(Heroes): Cid
    - Cid 의 cost formula 를 HeroDamageProperty 에 구현하여 삭제

--- src/main/java/org/cccccc/clickerheroes/hero/heroes/collections/Cid.java
+++ src/main/java/org/cccccc/clickerheroes/hero/heroes/collections/Cid.java
@@ -6,15 +6,6 @@ public class Cid extends AbstractHero {
     // starting hero
     public Cid(String damage, String cost) {
         super(damage, cost);
-        this.level = 1;
-    }
-
-    @Override
-    protected long levelUpCostFormula(int level) {
-        // C = min(5+level, 20) * (lv_cost_inc_ratio ^ level - 1) / (lv_cost_inc_ratio - 1)
-        return Math.min(5 + level, 20) * (long) Math.floor(
-                (Math.pow(LEVEL_UP_COST_INC_RATIO, level) - 1) / (LEVEL_UP_COST_INC_RATIO - 1)
-        );
     }
 
     @Override
